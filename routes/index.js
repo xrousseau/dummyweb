df79fc8ea470301d8416993f6d471d232c37a37a
David Rousseau <25441032+xrousseau@users.noreply.github.com>
2022-08-12 08:45:32 -0400

    jade test var

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var ip = require('ip');
 
+var pipo = "pipo";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: ip.address() });
+  res.render('index', { title: ip.address(), pipo });
 });
 
 module.exports = router;
