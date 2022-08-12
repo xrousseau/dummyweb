33a78cd446bd94c473eee04c5e0d7fcfc158e810
David Rousseau <25441032+xrousseau@users.noreply.github.com>
2022-08-12 11:17:48 -0400

    database connection

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var ip = require('ip');
+require('dotenv').config();
 
-var pipo = "pipo";
+const pgp = require('pg-promise')(/* options */)
+const db = pgp(`postgres://${process.env.PGUSR}:${process.env.PGPWD}@${process.env.PGSVR}:5432/${process.env.PGDB}`)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: ip.address(), pipo });
+  db.any('SELECT title FROM movies')
+  .then((data) => {
+    res.render('index', { title: ip.address(), data: data });
+    
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+    res.render('error', { error: error });
+  })
 });
 
 module.exports = router;
+
+
+
